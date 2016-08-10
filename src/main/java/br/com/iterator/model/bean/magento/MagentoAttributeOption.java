5f715ac1cb33331ebf416f6db4aa243f60316e19
Ricardo Auler Barrientos <desenvolvimento@iterator.com.br>
2016-08-09 23:21:04 -0300

    Commit complementar ao issue #4
    Criar processos para gerenciamento do Magento

---
+++ src/main/java/br/com/iterator/model/bean/magento/MagentoAttributeOption.java
@@ -0,0 +1,61 @@
+package br.com.iterator.model.bean.magento;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class MagentoAttributeOption implements Serializable {
+
+	private static final long serialVersionUID = 6680031602224079153L;
+	
+	private String label;
+	private String value;
+	
+	public String getLabel() {
+		return label;
+	}
+	
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+	
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((label == null) ? 0 : label.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MagentoAttributeOption other = (MagentoAttributeOption) obj;
+		if (label == null) {
+			if (other.label != null)
+				return false;
+		} else if (!label.equals(other.label))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+}
