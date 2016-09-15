134244e1ccc37df8b50adddecd2fdedae4f8c5aa
Ricardo Auler Barrientos <desenvolvimento@iterator.com.br>
2016-09-14 22:45:03 -0300

    Commit complementar ao issue #2
    Configuração do projeto

--- src/main/java/br/com/iterator/model/dao/InterfaceDAO.java
+++ src/main/java/br/com/iterator/model/dao/InterfaceDAO.java
@@ -3,6 +3,8 @@ package br.com.iterator.model.dao;
 import java.io.Serializable;
 import java.util.List;
 
+import br.com.iterator.model.bean.petcenterjau.Orcamento;
+
 public interface InterfaceDAO<T> {
 	void salvar(T bean);
 	void atualizar(T bean);
@@ -12,5 +14,7 @@ public interface InterfaceDAO<T> {
 	public List<T> getBeansMaxResults(Integer qtd);
 	List<T> getBeansByExample(T bean);
 	T getBeanByParametro(String parametro, String valor);
+	T getBeanByUltimoCodigo();
 	Integer somarEstoque(Integer produto);
+	List<T> getItensByOrcamento(Orcamento orcamento);
 }
